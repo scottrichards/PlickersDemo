b7cc8efa64279068f782cd8ae148a9cc206f254c
Scott Richards <scott.srichards@gmail.com>
2015-07-02 07:57:17 +0200

    Size Choices View Dynamically

--- PlickerDemo/ViewControllers/ChoicesTableViewController.m
+++ PlickerDemo/ViewControllers/ChoicesTableViewController.m
@@ -61,4 +61,53 @@
     return cell;
 }
 
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    // Label for Question Body Text
+    UILabel *questionBody = [[UILabel alloc] initWithFrame:CGRectMake(20, 12, tableView.frame.size.width-40, 100)];
+    [questionBody setFont:[UIFont systemFontOfSize:12]];
+    questionBody.text = _questionData.body;
+//    [headerView addSubview:questionBody];
+    questionBody.numberOfLines = 0;
+    [questionBody sizeToFit];
+    NSLog(@"Label's frame is: %@", NSStringFromCGRect(questionBody.frame));
+    float labelHeight = questionBody.frame.size.height;
+    
+    return 40 + labelHeight;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    // View for the header
+    UIView* headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 22)];
+    headerView.backgroundColor = [UIColor whiteColor];
+    
+    // Label for Question Body Text
+    UILabel *questionBody = [[UILabel alloc] initWithFrame:CGRectMake(20, 12, tableView.frame.size.width-40, 100)];
+    [questionBody setFont:[UIFont systemFontOfSize:12]];
+    questionBody.text = _questionData.body;
+    [headerView addSubview:questionBody];
+    questionBody.numberOfLines = 0;
+    [questionBody sizeToFit];
+    float labelHeight = questionBody.frame.size.height;
+    
+    // Label for Question Label
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, labelHeight + 20, 100, 18)];
+    [label setFont:[UIFont systemFontOfSize:15]];
+    label.text=@"Choice";
+    label.textColor = [UIColor lightGrayColor];
+    [headerView addSubview:label];
+    
+    // Label for Percent Complete
+    UILabel *percentComplete = [[UILabel alloc] initWithFrame:CGRectMake(tableView.frame.size.width - 60, labelHeight + 20, 60, 18)];
+    [percentComplete setFont:[UIFont systemFontOfSize:15]];
+    percentComplete.text=@"%";
+    percentComplete.textColor = [UIColor lightGrayColor];
+    [headerView addSubview:percentComplete];
+    
+    // 3. And return
+    return headerView;
+}
+
+
 @end
